ea53c147412f082c592bf30a75a2752ed0c051aa
PaulHofmeyr <psmhofmeyr@gmail.com>
2025-08-13 13:56:10 +0200

    Tokenize

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,91 @@
 #include <fstream>
 #include <sstream>
 #include <optional>
+#include <vector>
 
-enum class TokenType{
+enum class TokenType
+{
     _return,
     int_lit,
     semi
 };
 
-struct Token{
+struct Token
+{
     TokenType type;
     std::optional<std::string> value;
 };
 
-int main(int argc, char* argv[]){
-    if(argc != 2){
-        std::cerr << "Incorrect usage. Correct usage is: " << std::endl; 
+std::vector<Token> tokenize(const std::string &str)
+{
+    std::vector<Token> tokens;
+
+    std::string buffer;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char c = str.at(i);
+
+        if (std::isalpha(c))
+        {
+            buffer.push_back(c);
+            i++;
+            while (std::isalnum(str.at(i)))
+            {
+                buffer.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            if (buffer == "return")
+            {
+                tokens.push_back({.type = TokenType::_return});
+                buffer.clear();
+                continue;
+            }
+            else
+            {
+                std::cerr << "You messeed up!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (std::isdigit(c))
+        {
+            buffer.push_back(c);
+            while (std::isdigit(str.at(i)))
+            {
+                buffer.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            tokens.push_back({.type = TokenType::int_lit, .value = buffer});
+        }
+        else if (c == ';')
+        {
+            tokens.push_back({.type = TokenType::semi});
+        }
+        else if (std::isspace(c))
+        {
+            continue;
+        }
+        else
+        {
+            std::cerr << "You messeed up!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return tokens;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Incorrect usage. Correct usage is: " << std::endl;
         std::cerr << "PSCode <input.psc>" << std::endl;
         return EXIT_FAILURE;
     }
- 
+
     std::string contents;
     {
         std::stringstream contents_stream;
@@ -28,6 +94,8 @@ int main(int argc, char* argv[]){
         contents_stream << input.rdbuf();
         contents = contents_stream.str();
     }
+
+    std::vector<Token> thing = tokenize(contents);
 
     std::cout << contents << std::endl;
     return EXIT_SUCCESS;
