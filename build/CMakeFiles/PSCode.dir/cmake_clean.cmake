9e9684b8a4710688112d0058c2a565252d30b073
PaulHofmeyr <psmhofmeyr@gmail.com>
2025-08-13 13:26:07 +0200

    Setting up settings

---
+++ build/CMakeFiles/PSCode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSCode.dir/src/main.cpp.o"
+  "CMakeFiles/PSCode.dir/src/main.cpp.o.d"
+  "PSCode"
+  "PSCode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        std::cerr << "Incorrect usage. Correct usage is: " << std::endl; 
+        std::cerr << "PSCode <input.psc>" << std::endl;
+        return EXIT_FAILURE;
+    }
+ 
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    std::cout << contents << std::endl;
+    return EXIT_SUCCESS;
 }
